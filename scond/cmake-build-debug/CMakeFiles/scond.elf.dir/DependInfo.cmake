3f8db0039b6a3e40a69d9212d2efc1ba1c5264db
X-HUXI <X-HUXI@outlook.com>
2021-12-25 22:23:50 +0800

    CLion_LED

---
+++ scond/cmake-build-debug/CMakeFiles/scond.elf.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "F:/CLionProject/second/scond/startup/startup_stm32f103xb.s" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/CLionProject/second/scond/Core/Src/main.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Core/Src/main.c.obj"
+  "F:/CLionProject/second/scond/Core/Src/stm32f1xx_hal_msp.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "F:/CLionProject/second/scond/Core/Src/stm32f1xx_it.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "F:/CLionProject/second/scond/Core/Src/syscalls.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Core/Src/syscalls.c.obj"
+  "F:/CLionProject/second/scond/Core/Src/system_stm32f1xx.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "F:/CLionProject/second/scond/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "F:/CLionProject/second/scond/cmake-build-debug/CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ scond/cmake-build-debug/CMakeFiles/scond.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scond.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/scond.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/scond.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/scond.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/scond.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/scond.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/scond.elf.dir/startup/startup_stm32f103xb.s.obj"
+  "scond.elf"
+  "scond.elf.manifest"
+  "scond.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/scond.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
